d78ea89cebb7b2afe5c5d113695dae7a56a5cdcc
Nia <eugeniamashina@gmail.com>
2024-05-28 13:13:59 +0300

    fx

--- src/sections/how.jsx
+++ src/sections/how.jsx
@@ -138,7 +138,7 @@ export default function How() {
               let getParamsStr = window.location.search;
               const { aff_sub: click_id, aff_sub2: wm_id } = getGetParams();
               const data = {
-                landing: "2",
+                landing: "3",
                 email: emailValue,
                 phone_number: phoneValue,
                 wm_id: wm_id,
